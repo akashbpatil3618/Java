29a1633e8d4a95c75aa7cb522dd0ea9acc9fc29b
akashbpatil3618 <114017532+akashbpatil3618@users.noreply.github.com>
2022-09-20 23:12:18 +0530

    Add files via upload

---
+++ arry.java
@@ -0,0 +1,13 @@
+public class arry
+{
+public static void main(String [] args)
+{
+	int [] a={90,120,150,170,200,240,292};
+	
+	for(int i=0;i<a.length;i++)
+	{
+		System.out.println(a[i]);
+		
+	}
+}
+}
