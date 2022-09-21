5dc22fa438f60dbe428fa62e6fecb5097a0f12df
akashbpatil3618 <114017532+akashbpatil3618@users.noreply.github.com>
2022-09-21 20:29:24 +0530

    Add files via upload

---
+++ arry1.java
@@ -0,0 +1,16 @@
+public class arry1S
+{
+	public static void main(String [] args)
+	{
+		int[] a ={50,59,45,66,70};
+		
+		int sum=0;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			sum=sum+a[i];
+			
+		}
+		System.out.println(sum);
+	}
+}
