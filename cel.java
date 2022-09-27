ed7e771f2d0eab288dc371b7af196a039a1d4d31
akashbpatil3618 <114017532+akashbpatil3618@users.noreply.github.com>
2022-09-27 20:27:50 +0530

    Add files via upload

---
+++ cel.java
@@ -0,0 +1,12 @@
+public class cel
+{
+public static void main(String [] args)
+{float celsius = 13;
+myCode(celsius);
+}
+private static void myCode(float celsius)
+{float fahren = ((celsius*9)/5)+32;
+System.out.println("tem in fahrenhenit is: "+fahren);
+}
+}
+
