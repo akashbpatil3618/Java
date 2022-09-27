a0ce63e87958f8706dd7e7af6f8c045d14c8772c
akashbpatil3618 <114017532+akashbpatil3618@users.noreply.github.com>
2022-09-27 20:29:03 +0530

    Add files via upload

---
+++ chararry.java
@@ -0,0 +1,12 @@
+public class chararry
+{
+	public static void main(String [] args)
+	{
+		String s = "abcde";
+		char ch[] = s.toCharArray();
+		for(int i=0;i<ch.length;i++)
+		{
+			System.out.println(i+" "+ch[i]);
+		}
+	}
+}
