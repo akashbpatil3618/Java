d79dc9f278be77daffbbbd0d93ae4ae6fad04bcc
akashbpatil3618 <114017532+akashbpatil3618@users.noreply.github.com>
2022-10-01 19:57:26 +0530

    Add files via upload

---
+++ dessendsort.java
@@ -0,0 +1,32 @@
+/*
+to
+java
+welcome
+programming
+*/
+public class dessendsort
+{
+	public static void main(String [] args)
+	{
+		String s1="welcome to java programming";
+		
+		String [] sa=s1.split(" ");
+		
+		for(int i=0;i<sa.length;i++)
+		{
+			for(int j=i+1;j<sa.length;j++)
+			{
+				if(sa[i].length() > sa[j].length())
+				{
+					String temp=sa[i];
+					sa[i]=sa[j];
+					sa[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<sa.length;i++)
+		{
+			System.out.println(sa[i]);
+		}
+	}
+}
