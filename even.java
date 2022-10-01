1369e1b1d7a9c6fae003d9bf4e11f29d8adc751d
akashbpatil3618 <114017532+akashbpatil3618@users.noreply.github.com>
2022-10-01 19:58:19 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,23 @@
+public class even
+{
+public static void main(String [] args)
+{
+myCode();
+}
+public static void myCode()
+{ 
+int i =0;
+int a=20;
+for(i=0;i<=20;i++)
+{
+	if(i % 2==0)
+		System.out.println("even number "+i);
+	else if ( i % 2 != 0)
+		System.out.println("odd number "+i);
+	
+}
+}
+
+
+
+}
